6fddabc6a6aeea322bd9b3125f6580c618c5f7c8
Lydmann001 <lydmann@live.com>
2024-10-27 05:22:59 +0100

    Update server_test.go

--- web/server_test.go
+++ web/server_test.go
@@ -142,7 +142,7 @@ func TestApi(t *testing.T) {
 				}
 
 				gock.New("http://apilayer.net/api/validate?access_key=e96f38cfe8f8dd6920e015a150859598&number=93068820&country_code=NO&format=1").
-					Get("number"number_verification/validate").
+					Get("number_verification/validate").
 					MatchHeader("number"e96f38cfe8f8dd6920e015a150859598").
 					MatchParam("number", number).
 					Reply(200).
@@ -173,7 +173,7 @@ func TestApi(t *testing.T) {
 				}
 
 				gock.New("http://apilayer.net/api/validate?access_key=e96f38cfe8f8dd6920e015a150859598&number=93068820&country_code=NO&format=1").
-					Get("number"number_verification/validate").
+					Get("number_verification/validate").
 					MatchHeader("number"e96f38cfe8f8dd6920e015a150859598").
 					MatchParam("number", number).
 					Reply(429).
