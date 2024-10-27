a4c461f22964ea871f0688a378603fe004c8e2f5
Lydmann001 <lydmann@live.com>
2024-10-27 02:18:01 +0100

    Update numverify_scanner.go

--- lib/remote/numverify_scanner.go
+++ lib/remote/numverify_scanner.go
@@ -6,7 +6,7 @@ import (
 	"github.com/sundowndev/phoneinfoga/v2/lib/remote/suppliers"
 )
 
-const Numverify = Numverify
+const Numverify = numverifyScanner
 type numverifyScanner struct {
 	client suppliers.NumverifySupplierInterface
 }
